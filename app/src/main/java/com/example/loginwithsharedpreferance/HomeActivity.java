ffff798f8cfcc857ba58265d2d30f205c35b739f
Priyanka <ni3priyab3patil@gmail.com>
2022-08-13 22:37:47 +0530

    Login with SharedPreferance

---
+++ app/src/main/java/com/example/loginwithsharedpreferance/HomeActivity.java
@@ -0,0 +1,57 @@
+package com.example.loginwithsharedpreferance;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private TextView textView;
+    private PreferenceHandler preferenceHandler;
+    private static final String KEY_USER = "com.example.priyanka.USER_KEY1";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        textView = (TextView) findViewById(R.id.textView);
+        if (getIntent().getStringExtra(KEY_USER) == null) {
+            textView.setText("Welcome User");
+        } else {
+            textView.setText("Welcome " + getIntent().getStringExtra(KEY_USER));
+        }
+
+        preferenceHandler = new PreferenceHandler(getApplicationContext());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_file, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.logout) {
+            preferenceHandler.writeLoginStatus(false);
+            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
